c11db002454224cd3806705c1f858bf61f80697f
pietombi <ptombaccini@gmail.com>
2025-11-24 21:41:00 +0100

    allocPcb and emptyProcQ

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -12,12 +12,38 @@ void freePcb(pcb_t* p) {
 }
 
 pcb_t* allocPcb() {
+    // se la lista è vuota ritorno NULL
+    if(list_empty(&pcbFree_h)) {
+        return NULL;
+    
+    }
+    // altrimenti prendo un elemento dalla lista, lo rimuovo e lo ritorno
+    // prendo il primo elemento della lista
+    struct list_head* first = pcbFree_h.next;
+    // rimuovo l'elemento dalla lista
+    list_del(first);
+    // Come se dicessi "quale struttura pcb_t contiene questo campo p_list?" 
+    // -> ci restituisce il puntatore al pcb_t che dobbiamo ritornare
+    pcb_t* p = container_of(first, pcb_t, p_list);
+    
+    // inizializzo i campi del PCB
+    p->p_parent = NULL;
+    INIT_LIST_HEAD(&p->p_child);
+    INIT_LIST_HEAD(&p->p_sib);
+    p->p_s = 0;
+    p->p_time = 0;
+    p->p_semAdd = NULL;
+    p->p_supportStruct = NULL;
+    p->p_prio = 0;
+    p->p_pid = next_pid++;
+    return p;
 }
 
 void mkEmptyProcQ(struct list_head* head) {
 }
 
 int emptyProcQ(struct list_head* head) {
+    return list_empty(head);
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p) {
