d5141cbbd98205cec9a32ebafe918691cffc5d44
belbu <belbustiluca.05@gmail.com>
2026-03-02 20:02:13 +0100

    tolto extern da var globali

--- phase2/headers/initial.h
+++ phase2/headers/initial.h
@@ -1,12 +1,12 @@
 #include "../../headers/types.h"
 
-extern int processCount;
-extern int softBlockCount;
-extern struct list_head readyQueue;
-extern pcb_t *currentProcess;
-extern cpu_t start_time_current_quantum; // Variabile globale per memorizzare quanto tempo un processo rimane nella CPU
+ int processCount;
+ int softBlockCount;
+ struct list_head readyQueue;
+ pcb_t *currentProcess;
+ cpu_t start_time_current_quantum; // Variabile globale per memorizzare quanto tempo un processo rimane nella CPU
 /* Device semaphore */
-extern int deviceSemaphores[SEMDEVLEN];
+ int deviceSemaphores[SEMDEVLEN];
 
 void updateCPUTime(pcb_t *p);
 pcb_t* findProcessByPID(int pid);
