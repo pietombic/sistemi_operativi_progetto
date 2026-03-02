6de7964435dad4124451ec1163b62fbbc5eac2e0
pietombic <ptombaccini@gmail.com>
2026-03-02 20:24:30 +0100

    removed build folder

--- build/CMakeFiles/MultiPandOS.dir/klog.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MultiPandOS.dir/klog.c.o: \
- /home/belbu/Desktop/sistemi_operativi_progetto/klog.c
--- build/CMakeFiles/MultiPandOS.dir/phase2/p2test.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/MultiPandOS.dir/phase2/p2test.c.o: \
- /home/belbu/Desktop/sistemi_operativi_progetto/phase2/p2test.c \
- /home/belbu/Desktop/sistemi_operativi_progetto/phase2/../headers/const.h \
- /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
- /home/belbu/Desktop/sistemi_operativi_progetto/phase2/../headers/types.h \
- /usr/include/uriscv/types.h \
- /home/belbu/Desktop/sistemi_operativi_progetto/phase2/../headers/./const.h \
- /home/belbu/Desktop/sistemi_operativi_progetto/phase2/../headers/./listx.h \
- /usr/include/uriscv/liburiscv.h
